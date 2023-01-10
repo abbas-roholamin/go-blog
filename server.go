9f180c2ba6a5e23968e21748ee570e841de66091
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-01-10 20:49:22 +0430

    Update server.go

--- server.go
+++ server.go
@@ -10,16 +10,11 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/joho/godotenv"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
